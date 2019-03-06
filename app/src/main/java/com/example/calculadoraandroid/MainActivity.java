638760c4d4539a8e85bd2fa049ae94908abbd7a1
Yuuba <32301165+yuba17@users.noreply.github.com>
2019-03-06 16:12:51 +0100

    Imagen Inicial
    Calculadora con la función de sumar.

---
+++ app/src/main/java/com/example/calculadoraandroid/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.calculadoraandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txt_num1);
+        et2 = (EditText)findViewById(R.id.txt_num2);
+        tv1 = (TextView)findViewById(R.id.txt_resultado);
+
+    }
+
+    //Este método realiza la suma
+    public void Sumar(View view){
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+
+        //Diferencia entre parsear y casting ¿?
+        int num1 = Integer.parseInt(valor1);
+        int num2 = Integer.parseInt(valor2);
+
+        int suma = num1+num2;
+
+        String resultado = String.valueOf(suma);
+        tv1.setText(resultado);
+
+
+    }
+}
